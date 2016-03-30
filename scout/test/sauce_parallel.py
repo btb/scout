b61b7ea41e9ecc751f6b079f3ea845a03bb47ac9
Charlon Palacay <charlon@gmail.com>
2016-03-30 10:57:41 -0700

    Fixes pep8 errors.

--- scout/test/sauce_parallel.py
+++ scout/test/sauce_parallel.py
@@ -9,16 +9,20 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-import time, unittest, sys, copy
+import time
+import unittest
+import sys
+import copy
 import wd.parallel
-
+from sauceclient import SauceClient
 from django.conf import settings
 
 USERNAME = getattr(settings, 'SAUCE_USERNAME', False)
 ACCESS_KEY = getattr(settings, 'SAUCE_ACCESS_KEY', False)
 
-from sauceclient import SauceClient
+
 sauce_client = SauceClient(USERNAME, ACCESS_KEY)
+
 
 class SauceParallel(unittest.TestCase):
 
@@ -32,7 +36,6 @@ class SauceParallel(unittest.TestCase):
         browser['platform'] = 'OS X 10.11'
         browser['name'] = 'safari'
         browser['version'] = '9.0'
-        #browser['tags'] = "Parallel"
         browser['build'] = "story/food"
         desired_capabilities += [browser]
 
@@ -40,15 +43,13 @@ class SauceParallel(unittest.TestCase):
         browser['platform'] = 'OS X 10.10'
         browser['name'] = 'iPhone 6'
         browser['version'] = '9.2'
-        #browser['tags'] = "Parallel"
         browser['build'] = "story/food"
         desired_capabilities += [browser]
-        
+
         browser = copy.copy(webdriver.DesiredCapabilities.CHROME)
         browser['platform'] = 'OS X 10.11'
         browser['name'] = 'chrome'
         browser['version'] = '48.0'
-        #browser['tags'] = "Parallel"
         browser['build'] = "story/food"
         desired_capabilities += [browser]
 
@@ -56,7 +57,6 @@ class SauceParallel(unittest.TestCase):
         browser['platform'] = 'Windows 10'
         browser['name'] = 'firefox'
         browser['version'] = '44.0'
-        #browser['tags'] = "Parallel"
         browser['build'] = "story/food"
         desired_capabilities += [browser]
 
@@ -64,27 +64,29 @@ class SauceParallel(unittest.TestCase):
         browser['platform'] = 'Windows 10'
         browser['name'] = 'MicrosoftEdge'
         browser['version'] = '13.10586'
-        #browser['tags'] = "Parallel"
         browser['build'] = "story/food"
         desired_capabilities += [browser]
 
+        commmand_executor = "http://%s:%s@ondemand.saucelabs.com:80/wd/hub" % (
+            USERNAME, ACCESS_KEY)
         self.drivers = wd.parallel.Remote(
             desired_capabilities=desired_capabilities,
-            command_executor="http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub")
+            command_executor=command_executor)
 
     @wd.parallel.multiply
     def test_URL(self):
 
         self.baseurl = 'http://curry.aca.uw.edu:8001/'
 
-        sauce_client.jobs.update_job(self.driver.session_id, name="Test URL navigation")
-        #HomePage
+        sauce_client.jobs.update_job(self.driver.session_id,
+                                     name="Test URL navigation")
+        # HomePage
         self.driver.get(self.baseurl)
-        #FoodPage
+        # FoodPage
         self.driver.get(self.baseurl + 'food/')
-        #Discover Page
+        # Discover Page
         self.driver.get(self.baseurl + 'discover/')
-        #Filter Page
+        # Filter Page
         self.driver.get(self.baseurl + 'filter/')
 
     @wd.parallel.multiply
@@ -92,9 +94,11 @@ class SauceParallel(unittest.TestCase):
         print("https://saucelabs.com/jobs/%s \n" % self.driver.session_id)
         try:
             if sys.exc_info() == (None, None, None):
-                sauce_client.jobs.update_job(self.driver.session_id, passed=True)
+                sauce_client.jobs.update_job(self.driver.session_id,
+                                             passed=True)
             else:
-                sauce_client.jobs.update_job(self.driver.session_id, passed=False)
+                sauce_client.jobs.update_job(self.driver.session_id,
+                                             passed=False)
         finally:
             self.driver.quit()
 
