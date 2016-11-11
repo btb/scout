f033129a95590753eec37f15353be345b9532787
Bradley Bell <bradleyb@uw.edu>
2016-11-10 18:16:29 -0800

    Define app_name in travis project

--- travis-ci/urls.py
+++ travis-ci/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from scout import urls as scout_urls
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^', include('scout.urls')),
+    url(r'^', include(scout_urls, 'scout', scout_urls.app_name)),
 )
